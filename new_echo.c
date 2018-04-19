b48ace1bb92571470bd720fb52f478d36e9b2d27
nkmquan <nkmquan@debian-9.3.0-template>
2018-04-19 13:03:20 -0700

    Update new_echo program

--- new_echo.c
+++ new_echo.c
@@ -1,13 +1,26 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[])
-{
-	if (argc <= 1)
+{	
+	
+	int i;
+	if (argc < 2) //if user does not pass in any [agr]
 	{
 		printf("Usage ./new_echo [arg] [arg] ... [arg] \n");
 		return 0;
 	}
-
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			printf("%s", argv[i]); //print [agr] from user
+			printf(" ");			
+		}
+	
+		printf("\n");	
+	}
+	
 	return 0;
 }
