14d1335440ce64a10aae49359956303e3be8e245
Nguyen <nkmquan@debian-9.3.0-template>
2018-04-19 09:29:29 -0700

    Complete the new_cat.c program

---
+++ new_cat.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<string.h>
+#define MAX_FILE_NAME_CHARS 255
+int main(int argc, char *argv[])
+{
+	FILE *fp;
+	char file_name[MAX_FILE_NAME_CHARS];
+	char ch; 
+	int i;
+	   
+	if(argc < 1)
+	{
+		printf("Usage mycat <filename> \n");
+		return 0;
+	}
+	else if (argc > 1)
+	{
+		printf("Cannot open more than 1 file at a time");
+		return 0;
+	}
+
+	for(i = 1; i <= argc; i++)
+	{
+		strncpy(file_name, argv[i], MAX_FILE_NAME_CHARS);
+		fp = fopen(file_name, "r"); //open and read from the file
+
+		if(fp == NULL) //if the file does not exist
+		{
+	        	printf("%s: No such file or directory\n", file_name);
+			return 0;
+		}
+
+	        while((ch = fgetc(fp)) != EOF) //while it is not the end of file
+		{
+			putchar(ch);
+		}
+
+		fclose(fp);
+
+		fflush(stdout);
+	}
+
+	 return 0;
+}
