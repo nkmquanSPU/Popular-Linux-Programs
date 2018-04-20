0a3c95a743a94419040a29efd7f84e8d5d3674de
nkmquan <nkmquan@spu.edu>
2018-04-20 15:49:01 -0700

    Update the new_wc program to make it work when user switches the order of [-l|c] and [file.txt]

--- new_wc.c
+++ new_wc.c
@@ -12,6 +12,7 @@ int main(int argc, char *argv[])
 	char flag[3];
 	char l_flag[3] = "-l";
 	char c_flag[3] = "-c";
+	char temp_flag[MAX_FILE_NAME_CHARS];
 	char l;
 	char c;
 	char ch;
@@ -28,51 +29,100 @@ int main(int argc, char *argv[])
 		}
 		return 0;
 	}
-	if(argc != 3)
+	else if (argc == 2) //when file name is given; but flag is not
+	{
+		//get the file name from user
+		strncpy(file_name, argv[1], MAX_FILE_NAME_CHARS);
+		
+		fp = fopen(file_name, "r");
+		
+		//count words
+		while((ch=getc(fp)) != EOF)
+			if(ch == ' ' || ch == '\n')
+				word_count++;
+			
+		fclose(fp);
+		
+		//print out number of words
+		printf("%d \n", word_count);
+	}
+	else if (argc == 3) //when both file name and flag are given
+	{
+		strncpy(temp_flag, argv[1], MAX_FILE_NAME_CHARS); //take the second arg
+		
+		if(strlen(temp_flag) == 2) //if user enters ./new_wc [-l|c] [file.txt]
+		{
+			//get the file name from user
+			strncpy(file_name, argv[2], MAX_FILE_NAME_CHARS);
+			
+			fp = fopen(file_name, "r");
+			
+			if(fp == NULL) //if the file does not exist
+			{
+				printf("%s: No such file or directory.\n", file_name);
+				return 0;
+			}
+			
+			fp = fopen(file_name, "r");
+			
+			//count words
+			while((ch=getc(fp)) != EOF)
+				if(ch == ' ' || ch == '\n')
+					word_count++;
+			
+			fclose(fp);
+						
+			strncpy(flag, argv[1], 3); //take flag specified by user		
+		}
+		else //if user enters ./new_wc [file.txt] [-l|c] 
+		{
+			//get the file name from user
+			strncpy(file_name, argv[1], MAX_FILE_NAME_CHARS);
+			
+			fp = fopen(file_name, "r");
+			
+			if(fp == NULL) //if the file does not exist
+			{
+				printf("%s: No such file or directory.\n", file_name);
+				return 0;
+			}
+			
+			fp = fopen(file_name, "r");
+			
+			//count words
+			while((ch=getc(fp)) != EOF)
+				if(ch == ' ' || ch == '\n')
+					word_count++;
+			
+			fclose(fp);
+									
+			strncpy(flag, argv[2], 2); //take flag specified by user			
+		}
+		
+		fp = fopen(file_name, "r"); //re-open the file
+		
+		//process the file based on the given flag
+		if(strcmp(flag, l_flag) == 0) //if -l flag is given
+		{    					
+			//count the number of lines in the text file
+			for (l = getc(fp); l != EOF; l = getc(fp))
+				if (l == '\n')
+					line_count = line_count + 1; //increment count if we hit new line character
+			printf("%d %d \n", line_count, word_count);
+		} 
+		else if (strcmp(flag, c_flag) == 0) //if -c flag is given
+		{			
+			while((c = fgetc(fp)) != EOF) //while it is not the end of file		
+				char_count++;
+			printf("%d %d \n", char_count, word_count);
+		}			
+	}
+	else
 	{
 		printf("Usage ./new_wc [-l|c] [file.txt] \n");
 		return 0;
 	}
-	
-	//get the file name from user
-	strncpy(file_name, argv[2], MAX_FILE_NAME_CHARS);
-
-	fp = fopen(file_name, "r");
-	
-	if(fp == NULL) //if the file does not exist
-	{
-		printf("%s: No such file or directory.\n", file_name);
-		return 0;
-	}
-	
-	//for the -l flag
-	strncpy(flag, argv[1], 3); //take flag specified by user	
-	
-	
-	//count words
-	while((ch=getc(fp)) != EOF)
-		if(ch == ' ' || ch == '\n')
-			word_count++;
-
-	fclose(fp);
-
-	fp = fopen(file_name, "r");
-	if(strcmp(flag, l_flag) == 0) //if -l flag is given
-	{    	
-		
-		//count the number of lines in the text file
-		for (l = getc(fp); l != EOF; l = getc(fp))
-			if (l == '\n')
-				line_count = line_count + 1; //increment count if we hit new line character
-		printf("%d %d \n", line_count, word_count);
-	} 
-	else if (strcmp(flag, c_flag) == 0) //if -c flag is given
-	{
-	
-		while((c = fgetc(fp)) != EOF) //while it is not the end of file		
-			char_count++;
-		printf("%d %d \n", char_count, word_count);
-	}
+			
 	
 	fclose(fp); //close the file
 	return 0;
